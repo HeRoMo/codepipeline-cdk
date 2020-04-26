99e21bbcbcaac67474cbfa06d3162074b49bd3e2
HeRoMo <hero@asterisk-works.jp>
2020-04-26 18:23:28 +0900

    show config

--- bin/codepipeline-cdk.ts
+++ bin/codepipeline-cdk.ts
@@ -5,6 +5,10 @@ import * as cdk from '@aws-cdk/core';
 import { VpcStack } from '../lib/VpcStack';
 import { CodePipelineStack } from '../lib/CodePipelineStack';
 
+import { CONFIG } from '../lib/Config';
+
+console.info(JSON.stringify({ CONFIG }, null, 2));
+
 const app = new cdk.App();
 const vpcStack = new VpcStack(app, 'VpcStack');
 new CodePipelineStack(app, 'CodePipelineStack', vpcStack.vpc);
