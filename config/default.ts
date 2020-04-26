bcbd217971141f726c620dce6a205f2aaacf351a
HeRoMo <hero@asterisk-works.jp>
2020-04-26 17:40:39 +0900

    add config properties
    aws.secretManager
    github

--- config/default.ts
+++ config/default.ts
@@ -5,6 +5,14 @@ const config: ConfigDefinition = {
   aws: {
     account: process.env.AWS_ACCOUNT || '',
     region: 'ap-northeast-1',
+    secretManager: {
+      name: process.env.AWS_SECRET_MANAGER_NAME || '',
+    },
+  },
+  github: {
+    owner: 'HeRoMo',
+    repo: 'codepipeline-cdk',
+    branch: 'master',
   },
 };
 
--- lib/Config.ts
+++ lib/Config.ts
@@ -3,16 +3,27 @@ import * as config from 'config';
 export interface ConfigDefinition {
   appName: string;
   aws: AwsConfig;
+  github: GithubConfig;
 }
 
 interface AwsConfig {
   account: string;
   region: string;
+  secretManager: {
+    name: string;
+  }
+}
+
+interface GithubConfig {
+  owner: string;
+  repo: string;
+  branch: string;
 }
 
 const CONFIG: ConfigDefinition = {
-  appName: config.get<string>('projectName'),
+  appName: config.get<string>('appName'),
   aws: config.get<AwsConfig>('aws'),
+  github: config.get<GithubConfig>('github'),
 };
 
 export { CONFIG };
