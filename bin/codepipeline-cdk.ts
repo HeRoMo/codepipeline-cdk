4e76141827f192e864c558aeb3c36058f7c604b4
HeRoMo <hero@asterisk-works.jp>
2020-04-26 17:40:59 +0900

    add CodePipelineStack

--- bin/codepipeline-cdk.ts
+++ bin/codepipeline-cdk.ts
@@ -3,6 +3,8 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 
 import { VpcStack } from '../lib/VpcStack';
+import { CodePipelineStack } from '../lib/CodePipelineStack';
 
 const app = new cdk.App();
-new VpcStack(app, 'VpcStack');
+const vpcStack = new VpcStack(app, 'VpcStack');
+new CodePipelineStack(app, 'CodePipelineStack', vpcStack.vpc);
---
+++ lib/CodePipelineStack.ts
@@ -0,0 +1,82 @@
+import { Construct, SecretValue } from '@aws-cdk/core';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
+import { Artifact, Pipeline } from '@aws-cdk/aws-codepipeline';
+import {
+  BuildSpec,
+  ComputeType,
+  LinuxBuildImage,
+  PipelineProject,
+} from '@aws-cdk/aws-codebuild';
+import {
+  CodeBuildAction,
+  GitHubSourceAction,
+  GitHubTrigger,
+} from '@aws-cdk/aws-codepipeline-actions';
+
+import { BaseStack } from './BaseStack';
+import { CONFIG } from './Config';
+
+const {
+  appName,
+  github: { owner, repo, branch },
+  aws: {
+    secretManager,
+  },
+} = CONFIG;
+
+export class CodePipelineStack extends BaseStack {
+  public readonly vpc: Vpc;
+
+  constructor(scope: Construct, id: string, vpc: Vpc) {
+    super(scope, id);
+
+    this.vpc = vpc;
+
+    // Source Action
+    const sourceOutput = new Artifact();
+    const sourceAction = new GitHubSourceAction({
+      actionName: 'GitHubSource',
+      owner,
+      repo,
+      branch,
+      oauthToken: SecretValue.secretsManager(secretManager.name, { jsonField: 'GITHUB_TOKEN' }),
+      output: sourceOutput,
+      trigger: GitHubTrigger.WEBHOOK,
+    });
+
+    // CodeBuild Action
+    const buildArtifact = new Artifact();
+    const project = new PipelineProject(this, 'CodeBuild', {
+      projectName: `${appName}CodeBuild`,
+      description: `CodeBuild Project for ${appName}`,
+      buildSpec: BuildSpec.fromSourceFilename('buildspec.yml'),
+      environment: {
+        buildImage: LinuxBuildImage.STANDARD_4_0,
+        computeType: ComputeType.SMALL,
+        privileged: true,
+      },
+      subnetSelection: { subnetType: SubnetType.PRIVATE },
+      vpc: this.vpc,
+    });
+
+    const buildAction = new CodeBuildAction({
+      actionName: 'BuildAction',
+      project,
+      input: sourceOutput,
+      outputs: [buildArtifact],
+    });
+
+    new Pipeline(this, 'Pipeline', {
+      pipelineName: `${appName}Pipeline`,
+      stages: [
+        {
+          stageName: 'Source',
+          actions: [sourceAction],
+        }, {
+          stageName: 'Build',
+          actions: [buildAction],
+        },
+      ],
+    });
+  }
+}
